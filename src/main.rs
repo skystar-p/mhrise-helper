955f3d36700913402f840ccc96a5634239835a14
Jaehyeon Park <skystar@skystar.dev>
2022-07-10 21:39:50 +0900

    basic impl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::collections::HashMap;
+
+use itertools::iproduct;
+use types::ArmorPart;
+
+use crate::types::{Skill, Skills};
+
 mod crawl;
 mod types;
 
@@ -16,6 +23,71 @@ async fn main() -> anyhow::Result<()> {
         let s = tokio::fs::read_to_string("assets/decos.json").await?;
         serde_json::from_str(&s)?
     };
+
+    let wanted_skills = Skills::from_skills(&vec![
+        Skill {
+            name: "체술".to_string(),
+            level: 5,
+        },
+        Skill {
+            name: "스태미나 급속 회복".to_string(),
+            level: 3,
+        },
+        Skill {
+            name: "약점 특효".to_string(),
+            level: 3,
+        },
+        Skill {
+            name: "슈퍼회심".to_string(),
+            level: 2,
+        },
+    ]);
+
+    // create hashmap from armors, keyed by armor part
+    let mut armors_map = HashMap::new();
+    for armor in armors {
+        if armor.rarity < 7 {
+            continue;
+        }
+
+        let skills = Skills::from_skills(&armor.skills);
+        if !skills.has_intersection(&wanted_skills) {
+            continue;
+        }
+
+        let av = armors_map.entry(armor.part.clone()).or_insert(Vec::new());
+        av.push(armor.clone());
+    }
+
+    for (head, body, hands, waist, legs) in iproduct!(
+        armors_map.get(&ArmorPart::Head).unwrap(),
+        armors_map.get(&ArmorPart::Body).unwrap(),
+        armors_map.get(&ArmorPart::Hands).unwrap(),
+        armors_map.get(&ArmorPart::Waist).unwrap(),
+        armors_map.get(&ArmorPart::Legs).unwrap()
+    ) {
+        let skills = Skills::new();
+        let head_skills = Skills::from_skills(&head.skills);
+        let body_skills = Skills::from_skills(&body.skills);
+        let hands_skills = Skills::from_skills(&hands.skills);
+        let waist_skills = Skills::from_skills(&waist.skills);
+        let legs_skills = Skills::from_skills(&legs.skills);
+
+        // merge all
+        let skills = skills.merge(&head_skills);
+        let skills = skills.merge(&body_skills);
+        let skills = skills.merge(&hands_skills);
+        let skills = skills.merge(&waist_skills);
+        let skills = skills.merge(&legs_skills);
+
+        if skills.is_superset_of(&wanted_skills) {
+            println!("{:?}", skills);
+            println!(
+                "{} {} {} {} {}",
+                head.name, body.name, hands.name, waist.name, legs.name
+            );
+        }
+    }
 
     Ok(())
 }
--- src/types.rs
+++ src/types.rs
@@ -15,6 +15,68 @@ pub struct Skill {
     pub level: isize,
 }
 
+#[derive(Debug)]
+pub struct Skills(pub Vec<Skill>);
+
+impl Skills {
+    pub fn new() -> Self {
+        Skills(Vec::new())
+    }
+
+    pub fn from_skills(skills: &Vec<Skill>) -> Self {
+        Skills(skills.clone())
+    }
+
+    pub fn merge(&self, other: &Self) -> Self {
+        let mut res = other.0.clone();
+        for s in self.0.iter() {
+            // find the same skill in the other list
+            // if found, merge the level
+            let mut found = false;
+            for o in res.iter_mut() {
+                if s.name == o.name {
+                    found = true;
+                    o.level += s.level;
+                    break;
+                }
+            }
+            if !found {
+                res.push(s.clone());
+            }
+        }
+        Skills(res)
+    }
+
+    pub fn is_superset_of(&self, other: &Self) -> bool {
+        for o in other.0.iter() {
+            let mut ok = false;
+            for s in self.0.iter() {
+                if s.name == o.name {
+                    if s.level >= o.level {
+                        ok = true;
+                    }
+                    break;
+                }
+            }
+            if !ok {
+                return false;
+            }
+        }
+        true
+    }
+
+    pub fn has_intersection(&self, other: &Self) -> bool {
+        for s in self.0.iter() {
+            for o in other.0.iter() {
+                if s.name == o.name {
+                    return true;
+                }
+            }
+        }
+        false
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Hash, Clone)]
 #[serde(rename_all = "snake_case")]
 pub enum ArmorPart {
