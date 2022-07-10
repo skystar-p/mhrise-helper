741394a886131bd3764371c915e6457238ea8858
Jaehyeon Park <skystar@skystar.dev>
2022-07-10 19:08:20 +0900

    rename

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-mod parse;
+mod crawl;
 mod types;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let armors = if tokio::fs::metadata("assets/armors.json").await.is_err() {
-        parse::crawl_and_parse().await?
+        crawl::crawl_and_parse().await?
     } else {
         let s = tokio::fs::read_to_string("assets/armors.json").await?;
         serde_json::from_str(&s)?
